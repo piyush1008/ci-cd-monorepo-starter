4a5124b141bbaa00bf6f7b2c6fd65cd8cb695161
Piyush Dubey <piyushdubey@Piyushs-MacBook-Air.local>
2025-10-28 14:20:57 +0530

    making frontend changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home(){
   return(
     <div className="flex w-screen h-screen justify-center items-center">
       <div>
-        hello
+        hello there
         <input className="border-1 rounded-xl p-1" value={roomId} onChange={(e)=>{
             setRoomId(e.target.value)
         }} type="text" placeholder="Room id"></input>
